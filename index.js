cff3674637f129a995fab25724766c341e137929
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-05 04:12:30 +0300

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
-// retrieve data from json file
-fetch('flights.json')
+const tableBody = document.getElementById('table-body');
+
+fetch('http://localhost:3000/flights')
   .then(response => response.json())
   .then(data => {
-    // update available flights list
-    const availableFlights = document.getElementById('available-flights');
-    availableFlights.innerHTML = '';
     data.flights.forEach(flight => {
-      const listItem = document.createElement('li');
-      listItem.innerText = `${flight.flightNumber}: ${flight.origin} to ${flight.destination} at ${flight.departureTime}`;
-      availableFlights.appendChild(listItem);
+      const row = `
+        <tr>
+          <td>${flight.from}</td>
+          <td>${flight.to}</td>
+          <td>${flight.depart}</td>
+          <td>${flight.arrive}</td>
+          <td>${flight.price}</td>
+          <td>${flight.available_tickets}</td>
+          <td><img src="${flight.destination_picture}" alt="${flight.to}" width="200"></td>
+        </tr>
+      `;
+      tableBody.insertAdjacentHTML('beforeend', row);
     });
 
-    // update book a flight form
-    const bookFlightForm = document.getElementById('book-flight-form');
-    bookFlightForm.innerHTML = '';
-    const flightSelect = document.createElement('select');
-    flightSelect.setAttribute('id', 'flight');
-    flightSelect.setAttribute('name', 'flight');
-    data.flights.forEach(flight => {
-      const option = document.createElement('option');
-      option.setAttribute('value', flight.flightNumber);
-      option.innerText = `${flight.flightNumber}: ${flight.origin} to ${flight.destination} at ${flight.departureTime}`;
-      flightSelect.appendChild(option);
-    });
-    bookFlightForm.appendChild(flightSelect);
+    // Get list of flights
+    const flights = data.flights;
+
+    // Create list of flight names
+    const flightList = document.getElementById('flight-list');
+    for (let i = 0; i < flights.length; i++) {
+      const flightName = flights[i].from + ' to ' + flights[i].to;
+      const listItem = document.createElement('li');
+      listItem.textContent = flightName;
+      flightList.appendChild(listItem);
+
+      // Add click listener to show destination image
+      listItem.addEventListener('click', () => {
+        destinationImage.src = flights[i].destinationImage;
+      });
+    }
   })
-  .catch(error => {
-    console.error(error);
-  });
+  .catch(error => console.error(error));
+
+const destinationImage = document.getElementById('destination-image');
