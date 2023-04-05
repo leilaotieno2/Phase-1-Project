07eed336d37afd8b065f9822224a546a1e182332
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-05 06:56:49 +0300

    first commit

--- index.js
+++ index.js
@@ -1,47 +1,4 @@
-/*const destinationsList = document.getElementById("destinations-list");
-const destinationDetails = document.getElementById("destination-details");
 
-fetch("db.json")
-  .then(response => response.json())
-  .then(data => {
-    // create a list of destinations
-    data.flights.forEach(flight => {
-      const listItem = document.createElement("li");
-      listItem.textContent = flight.to;
-      destinationsList.appendChild(listItem);
-
-      // add event listener to show destination details
-      listItem.addEventListener("click", () => {
-        // clear previous details
-        destinationDetails.innerHTML = "";
-
-        // add destination image
-        const destinationImage = document.createElement("img");
-        destinationImage.src = flight.destination_picture;
-        destinationDetails.appendChild(destinationImage);
-
-        // add destination information
-        const destinationInfo = document.createElement("div");
-        destinationInfo.innerHTML = `
-          <h3>${flight.to}</h3>
-          <p>From: ${flight.from}</p>
-          <p>Departure: ${flight.depart}</p>
-          <p>Arrival: ${flight.arrive}</p>
-          <p>Price: $${flight.price}</p>
-          <p>Available Tickets: ${flight.available_tickets}</p>
-          <button class="book-button">Book Now</button>
-        `;
-        destinationDetails.appendChild(destinationInfo);
-
-        // add event listener to book button
-        const bookButton = destinationInfo.querySelector(".book-button");
-        bookButton.addEventListener("click", () => {
-          // code to book a ticket
-        });
-      });
-    });
-  })
-  .catch(error => console.log(error));*/
   const destinationsList = document.getElementById("destinations-list");
   const destinationDetails = document.getElementById("destination-details");
   const description = document.getElementById("description");
