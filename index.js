e75e27598ac16f0136887da87725d4d821edbc36
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-05 06:31:11 +0300

    first commit

--- index.js
+++ index.js
@@ -1,40 +1,107 @@
-const tableBody = document.getElementById('table-body');
+/*const destinationsList = document.getElementById("destinations-list");
+const destinationDetails = document.getElementById("destination-details");
 
-fetch('http://localhost:3000/flights')
+fetch("db.json")
   .then(response => response.json())
   .then(data => {
+    // create a list of destinations
     data.flights.forEach(flight => {
-      const row = `
-        <tr>
-          <td>${flight.from}</td>
-          <td>${flight.to}</td>
-          <td>${flight.depart}</td>
-          <td>${flight.arrive}</td>
-          <td>${flight.price}</td>
-          <td>${flight.available_tickets}</td>
-          <td><img src="${flight.destination_picture}" alt="${flight.to}" width="200"></td>
-        </tr>
-      `;
-      tableBody.insertAdjacentHTML('beforeend', row);
-    });
+      const listItem = document.createElement("li");
+      listItem.textContent = flight.to;
+      destinationsList.appendChild(listItem);
 
-    // Get list of flights
-    const flights = data.flights;
+      // add event listener to show destination details
+      listItem.addEventListener("click", () => {
+        // clear previous details
+        destinationDetails.innerHTML = "";
 
-    // Create list of flight names
-    const flightList = document.getElementById('flight-list');
-    for (let i = 0; i < flights.length; i++) {
-      const flightName = flights[i].from + ' to ' + flights[i].to;
-      const listItem = document.createElement('li');
-      listItem.textContent = flightName;
-      flightList.appendChild(listItem);
+        // add destination image
+        const destinationImage = document.createElement("img");
+        destinationImage.src = flight.destination_picture;
+        destinationDetails.appendChild(destinationImage);
 
-      // Add click listener to show destination image
-      listItem.addEventListener('click', () => {
-        destinationImage.src = flights[i].destinationImage;
+        // add destination information
+        const destinationInfo = document.createElement("div");
+        destinationInfo.innerHTML = `
+          <h3>${flight.to}</h3>
+          <p>From: ${flight.from}</p>
+          <p>Departure: ${flight.depart}</p>
+          <p>Arrival: ${flight.arrive}</p>
+          <p>Price: $${flight.price}</p>
+          <p>Available Tickets: ${flight.available_tickets}</p>
+          <button class="book-button">Book Now</button>
+        `;
+        destinationDetails.appendChild(destinationInfo);
+
+        // add event listener to book button
+        const bookButton = destinationInfo.querySelector(".book-button");
+        bookButton.addEventListener("click", () => {
+          // code to book a ticket
+        });
       });
-    }
+    });
   })
-  .catch(error => console.error(error));
-
-const destinationImage = document.getElementById('destination-image');
+  .catch(error => console.log(error));*/
+  const destinationsList = document.getElementById("destinations-list");
+  const destinationDetails = document.getElementById("destination-details");
+  const description = document.getElementById("description");
+  
+  fetch("db.json")
+    .then(response => response.json())
+    .then(data => {
+      // create a list of destinations
+      data.flights.forEach(flight => {
+        const listItem = document.createElement("li");
+        listItem.textContent = flight.to;
+        destinationsList.appendChild(listItem);
+  
+        // add event listener to show destination details
+        listItem.addEventListener("click", () => {
+          // clear previous details
+          destinationDetails.innerHTML = "";
+  
+          // add destination image
+          const destinationImage = document.createElement("img");
+          destinationImage.src = flight.destination_picture;
+          destinationDetails.appendChild(destinationImage);
+  
+          // add destination information
+          const destinationInfo = document.createElement("div");
+          destinationInfo.innerHTML = `
+            <h3>${flight.to}</h3>
+            <p>From: ${flight.from}</p>
+            <p>Departure: ${flight.depart}</p>
+            <p>Arrival: ${flight.arrive}</p>
+            <p>Price: $${flight.price}</p>
+            <p>Available Tickets: ${flight.available_tickets}</p>
+            <button class="book-button">Book Now</button>
+            <button class="cancel-button">Cancel Ticket</button>
+            <button class="rewards-button">View Rewards</button>
+          `;
+          destinationDetails.appendChild(destinationInfo);
+  
+          // add event listener to book button
+          const bookButton = destinationInfo.querySelector(".book-button");
+          bookButton.addEventListener("click", () => {
+            // code to book a ticket
+          });
+  
+          // add event listener to cancel button
+          const cancelButton = destinationInfo.querySelector(".cancel-button");
+          cancelButton.addEventListener("click", () => {
+            // code to cancel a ticket
+          });
+  
+          // add event listener to rewards button
+          const rewardsButton = destinationInfo.querySelector(".rewards-button");
+          rewardsButton.addEventListener("click", () => {
+            // code to view rewards
+          });
+  
+          // display description
+          description.innerHTML = flight.description;
+        });
+      });
+    })
+    .catch(error => console.log(error));
+  
